ef9de53edfc61afb3392ff231153198994fc6803
Kottuzik <ewsik.s@gmaill.com>
2020-04-10 15:40:21 +0300

    added material ui for the styles of menu and added my styles also

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import Header from './components/header/Header';
 import TasksNotes from './pages/TasksNotes/TasksNotes';
 import Users from './pages/users/Users';
 
+const items = [
+  { name: 'board', label: 'Board' },
+  { name: 'users', label: 'Users' },
+  { name: 'guest', label: 'Guest' },
+]
+
 const App = () => {
   return(
     <div className="wrapper">
-      <Menu />
+      <Menu items = {items} />
       <Header />
       <Users />
       {/* <Login /> */}
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 
 const Menu = () => {
     return(
-        <div className="menu">
-            <h1>Slobby</h1>
-            <ul>
-                <li>Board</li>
-                <li>Users</li>
-                <li>Guest</li>
-            </ul>
+        <div className="sidenav">
+            <List disablePadding dense>
+                <ListItem className="menuItem" button>
+                    <ListItemText>Board</ListItemText>
+                </ListItem>
+                <ListItem className="menuItem" button>
+                    <ListItemText>Users</ListItemText>
+                </ListItem>
+                <ListItem className="menuItem" button>
+                    <ListItemText>Guest</ListItemText>
+                </ListItem>
+            </List>
         </div>
     )
 }
