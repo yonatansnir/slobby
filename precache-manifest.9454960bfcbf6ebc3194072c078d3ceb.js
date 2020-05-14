ace7960a25bb8a6fa75833d55c75b3980424e228
Yonatan Snir <yonatan941@gmail.com>
2020-05-14 14:23:50 +0300

    Updates

---
+++ precache-manifest.9454960bfcbf6ebc3194072c078d3ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74b6acd3b1cab5bc6d9dd447217c41e",
+    "url": "/slobby/index.html"
+  },
+  {
+    "revision": "4be27a40ffe831ab5dec",
+    "url": "/slobby/static/css/main.156e38ee.chunk.css"
+  },
+  {
+    "revision": "651e7776b5eb27a3db62",
+    "url": "/slobby/static/js/2.7a2f0246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slobby/static/js/2.7a2f0246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be27a40ffe831ab5dec",
+    "url": "/slobby/static/js/main.c4717f8e.chunk.js"
+  },
+  {
+    "revision": "a6b8a0521219821fc5a5",
+    "url": "/slobby/static/js/runtime-main.44848229.js"
+  }
+]);
